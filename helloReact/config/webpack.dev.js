2b23c1af492164c917490d7253cfddb98c383e34
Rashad Chowdhury <78812401+rashad-moves@users.noreply.github.com>
2023-05-15 20:32:52 -0400

    update helloReact ;

---
+++ helloReact/config/webpack.dev.js
@@ -0,0 +1,32 @@
+const { merge } = require('webpack-merge');
+const HelloWebpackPlugin = require('html-webpack-plugin');
+const commonConfig = require('./webpack.common');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const packageJson = require('../package.json');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const devConfig = {
+    node: 'development',
+    output: {
+        publicPath: 'http://localhost:8081',
+    },
+    devServer: {
+        port: 8081,
+        historyApiFallback: {
+            index: '/index.html'
+        }
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'helloReact',
+            filname: 'remomteEntry.js',
+            exposes: {
+                './HelloReactApp': './src/bootstrap'
+            },
+            shared: packageJson.dependencies
+        }),
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
+}
